be1af535e6325d4e42376e009d99971a1293bdaf
c7w <admin@cc7w.cf>
2021-07-27 17:18:40 +0800

    (fix) access-control-allow-origin

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const port = 10001
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.post('/', (req, res) => {
     const path = req.body.path
     const data = req.body.data
